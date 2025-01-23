9349dbdab134070cd564a9e1040c073ed3918682
sumit <sumit821044@gmail.com>
2025-01-23 12:23:45 +0530

    Lec-8 - Understand form and their intraction

--- chai/urls.py
+++ chai/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.all_chai, name='allChai'),
     path('<int:chai_id>/', views.chai_detail, name='chai_detail'),
+    path("chai_stores/", views.chai_store_views, name='chai_store_details'),
 ]
